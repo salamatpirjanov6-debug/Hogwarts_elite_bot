f56619dd6f37d0eb819365d54f90b39be3ce2b43
salamatpirjanov6-debug <salamatpirjanov6@gmail.com>
2026-04-02 20:09:57 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,8 @@ GROUP = "@hogwarts_elite"
 ADMIN_ID = 7670992727
 
 if not API_TOKEN:
-("TELEGRAM_BOT_TOKEN")
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
 
 logging.basicConfig(level=logging.INFO)
 
