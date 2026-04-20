2a8084fdc42d1f099e32031b1147b24c241a48e7
salamatpirjanov6-debug <salamatpirjanov6@gmail.com>
2026-04-20 15:30:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ API_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "8679378410:AAGPBLgayAH0psOVxMf
 CHANNEL = "@SaIamatPirjanov"
 GROUP = "@SalamatPirjanov_chat"
 ADMIN_ID = 7821230725
-SHLYAPA_USER = "@N_e_w_t_Scamander"
+SHLYAPA_USER = "@8032130299"
 
 logging.basicConfig(level=logging.INFO)
 storage = MemoryStorage()
